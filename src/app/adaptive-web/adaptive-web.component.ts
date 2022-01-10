5a8feee63688f20f0ceb49e0984a064a04c80278
vyktor112 <victortatomir@gmail.com>
2022-01-10 12:57:18 +0200

    first commit

---
+++ src/app/adaptive-web/adaptive-web.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adaptive-web',
+  templateUrl: './adaptive-web.component.html',
+  styleUrls: ['./adaptive-web.component.css']
+})
+export class AdaptiveWebComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LectureComponent } from './lecture/lecture.component';
+import { AdaptiveWebComponent } from './adaptive-web/adaptive-web.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    AboutComponent,
+    LectureComponent,
+    AdaptiveWebComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
